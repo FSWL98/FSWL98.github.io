b3158980bc54314c94413ca23d76767bb2ed33f8
FSWL98 <34787209+FSWL98@users.noreply.github.com>
2019-04-03 18:41:13 +0700

    added text for lab3

--- Lab3/js/main.js
+++ Lab3/js/main.js
@@ -1,3 +1,4 @@
+var isDone = [0, 0, 0, 0];
 function randomSize (min, max) {
     return Math.floor(min+ + Math.random() * (max + 1 - min));
 }
@@ -5,13 +6,15 @@ function randomSize (min, max) {
 function generateSizesForCanvas(canvas) {
     return { x: randomSize(50, canvas.width - 50), y: randomSize(50, canvas.height - 50)};
 }
-function drawRandImage (canvas, context, x, y, width, height) {
+function drawRandImage (canvas, context, x, y, width, height, index) {
     var pic = new Image();
     pic.setAttribute('crossOrigin', 'anonymous');
     pic.onload = function () {
         context.drawImage(pic, x, y, width, height);
+        isDone[index] = 1;
+        getQuote(canvas, context, 30);
     }
-    pic.src = `https://source.unsplash.com/collection/1127163/${width}x${height}`;
+    pic.src = `https://source.unsplash.com/collection/1127160/${width}x${height}`;
 }
 
 function drawImages (canvas, context) {
@@ -21,13 +24,81 @@ function drawImages (canvas, context) {
     var XStartPoints = [0, 0, sizes.x, sizes.x];
     var YStartPoints = [0, sizes.y, 0, sizes.y];
     for (var i=0; i<4; i++) {
-        drawRandImage(canvas, context, XStartPoints[i], YStartPoints[i], widths[i], heights[i]);
-
+        drawRandImage(canvas, context, XStartPoints[i], YStartPoints[i], widths[i], heights[i], i);
     }
+
+}
+function getQuote(canvas, context, lineHeight ) {
+    if (!(isDone[0] && isDone[1] && isDone[2] && isDone[3])) {
+        return;
+    }
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://cors-anywhere.herokuapp.com/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=ru', true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState != 4) {
+            return;
+        }
+        if (xhr.status == 200) {
+
+            context.fillStyle = 'rgba(0, 0, 0, 0.7)';
+            context.fillRect(0, 0, canvas.width, canvas.height);
+            context.font ='bold 26px Arial';
+            context.fillStyle = 'white';
+            context.textAlign = 'center';
+            context.textBaseline = 'middle';
+            drawText(context, canvas, JSON.parse(xhr.responseText)['quoteText'], lineHeight);
+        }
+
+    };
+    xhr.send();
+}
+function linesForCanvas (context, text, maxWidth) {
+    var words = text.split(' ');
+    var lines = [];
+
+    var currentLine = '';
+
+    words.forEach(function (value) {
+        var newLine = currentLine + value + ' ';
+        var newLineWidth = context.measureText(newLine).width;
+
+        if (newLineWidth > maxWidth) {
+            lines.push(currentLine);
+            currentLine = value + ' ';
+        } else {
+            currentLine = newLine;
+        }
+    });
+
+    lines.push(currentLine);
+
+    return lines;
+}
+
+function drawText (context, canvas, text, lineHeight) {
+    var maxWidth = canvas.width - 40;
+    var maxHeight = canvas.height - 40;
+
+    var centerX = canvas.width / 2;
+
+    var lines = linesForCanvas(context, text, maxWidth);
+    var listHeight = lines.length * lineHeight;
+
+    if (listHeight > maxHeight) {
+        context.fillText('Text is too big', canvas.height / 2, canvas.width / 2);
+        return;
+    }
+
+    var marginTop = (maxHeight - listHeight) / 2;
+
+    lines.forEach(function (value){
+        context.fillText(value, centerX, marginTop);
+        marginTop += lineHeight;
+    });
 }
 function createCanvas(width, height) {
     var canvas = document.createElement('canvas');
-    canvas.id = 'canvasD';
+    canvas.id = "canvasD";
     var context = canvas.getContext('2d');
     canvas.width = width;
     canvas.height = height;
